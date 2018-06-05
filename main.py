b9b05153e4d3eedf17f8cd2047abdf9dccbdf8eb
Matheus Valenza <matheusvvalenza@gmail.com>
2018-06-05 17:01:08 -0300

    Correção equação de Bezier

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ def main():
 
 	x = y = 0
 	running = 1
-	screen = pygame.display.set_mode((640, 400))
+	screen = pygame.display.set_mode((1200, 600))
 	screen.fill(BACKGROUND_COLOR)
 	pygame.display.flip()
 
 	clicked_points = []
+	curves = []
  
 	while running:
 		event = pygame.event.poll()
@@ -24,11 +25,14 @@ def main():
 			pygame.draw.circle(screen, POINT_COLOR, event.pos, 4)
 			print(clicked_points)
 		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_SPACE:
+			if chr(event.key) == "p" and len(clicked_points) == 5:
 				draw_polygon(screen, clicked_points)
-			elif chr(event.key) == "b":
+			elif chr(event.key) == "b" and len(clicked_points) == 5:
 				print ("draw bezier")
-				draw_bezier(screen, clicked_points)
+				curve = draw_bezier(screen, clicked_points)
+				curves.append(curve)
+				clicked_points = []
+				print ("len(curves): ", len(curves))
 		
 		pygame.display.flip()
 
@@ -39,11 +43,16 @@ def draw_bezier(screen, clicked_points):
 	precision = 0.001
 	t = 0.0
 
+	c1 = []
+
 	while (t < 1):
 		point = bezier_function(t, clicked_points)
+		c1.append(point)
 		pygame.draw.circle(screen, POINT_COLOR, point, 2)
 		t += precision
 		pygame.display.flip()
+
+	return c1
 
 def bezier_function(t, clicked_points):
 	B0 = clicked_points[0]
@@ -58,10 +67,10 @@ def bezier_function(t, clicked_points):
 	print (B3)
 	print (B4)
 
-	x = (1 - t)**4*B0[0] + 4*t*(1 - t)**3*B1[0] + 4*t**2*(1 - t)*B2[0] + 4*t**3*(1 - t)*B3[0] + t**4*B4[0]
+	x = (1 - t)**4*B0[0] + 4*t*(1 - t)**3*B1[0] + 6*t**2*(1 - t)**2*B2[0] + 4*t**3*(1 - t)*B3[0] + t**4*B4[0]
 	print ("x: ", x)
 
-	y = (1 - t)**4*B0[1] + 4*t*(1 - t)**3*B1[1] + 4*t**2*(1 - t)*B2[1] + 4*t**3*(1 - t)*B3[1] + t**4*B4[1]
+	y = (1 - t)**4*B0[1] + 4*t*(1 - t)**3*B1[1] + 6*t**2*(1 - t)**2*B2[1] + 4*t**3*(1 - t)*B3[1] + t**4*B4[1]
 	print ("y: ", y)
 
 	return int(x), int(y)
